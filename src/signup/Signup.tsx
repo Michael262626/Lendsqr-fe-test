3e29f0bf5e8e04925d4c2612968510e074ab58f5
michael <dikandumichael@gmail.com>
2024-09-27 00:54:57 +0100

    Sign up page designed

---
+++ src/signup/Signup.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../src/images/Group.svg";
+import image from "../../src/images/pablo-sign-in 1.svg";
+import "./input.scss";
+
+const Signup: React.FC = () => {
+    return (
+        <div className="logo">
+            <div>
+                <img src={logo} alt="Logo" />
+                <div className="image">
+                    <img src={image} alt="Sign In Illustration" />
+                </div>
+            </div>
+            <div className="heading">
+                <h1>Welcome!</h1>
+                <p>Enter details to login</p>
+                <div>
+                    <input type="email" placeholder="Email" className="input"/>
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" className="input"/>
+                </div>
+                <h6>FORGOT PASSWORD?</h6>
+                <button className="btn">Log in</button>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
