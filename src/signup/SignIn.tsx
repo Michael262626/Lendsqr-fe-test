1d3c0f979e2fdb7c289ae76c023ab485b323e199
michael <dikandumichael@gmail.com>
2024-09-27 02:58:23 +0100

    Added the toggle icon to show and hide password

--- src/signup/SignIn.tsx
+++ src/signup/SignIn.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../src/images/Group.svg";
 import image from "../../src/images/pablo-sign-in 1.svg";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"; // Import show/hide icons from react-icons
 import "./input.scss";
 
 const SignIn: React.FC = () => {
+    const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword); // Toggle between true and false
+    };
     return (
         <div className="logo">
             <div>
@@ -18,8 +24,16 @@ const SignIn: React.FC = () => {
                 <div>
                     <input type="email" placeholder="Email" className="input"/>
                 </div>
-                <div>
-                    <input type="password" placeholder="Password" className="input"/>
+                <div className="input-container">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password"
+                        className="input"
+                    />
+                    <span className="toggle-icon" onClick={togglePasswordVisibility}>
+                        {showPassword ? <AiFillEyeInvisible/> :
+                            <AiFillEye/>}
+                    </span>
                 </div>
                 <h6>FORGOT PASSWORD?</h6>
                 <button className="btn">Log in</button>
